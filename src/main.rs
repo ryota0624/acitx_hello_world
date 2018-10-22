00dea8141b3cf0a4974b48af34ebd8045f7a4919
ryota suzuki <ryota@fringe81.com>
2018-10-22 11:10:58 +0900

    configを豪華にしてみる

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ extern crate lazy_static;
 use actix_web::{http, server, App, HttpRequest};
 use actix_web::middleware::Logger;
 
-use actix_helloworld::settings::{Settings};
-use log::{info};
+use actix_helloworld::settings::Settings;
+use log::info;
 use simplelog::*;
 
 #[derive(Clone)]
@@ -38,8 +38,8 @@ impl UseSetting for SettingProvider {
 }
 
 trait EchoServerNameService: UseSetting {
-    fn echo(&self) {
-        info!("server name -> {:?}", self.settings());
+    fn echo(&self) -> String {
+        format!("my name is {}", self.settings().server_name)
     }
 }
 
@@ -59,18 +59,23 @@ fn main() {
             TermLogger::new(LevelFilter::Info, Config::default()).unwrap()
         ]
     ).unwrap();
-    EchoServerNameServiceImpl.echo();
 
-    server::new(
+    let server_setting = global_settings.0.server.clone();
+    info!("server setting {:?}", global_settings.0);
+
+    let app_server = server::new(
         || App::new()
             .middleware(Logger::default())
             .route("/health", http::Method::GET, |_: HttpRequest| "OK")
+            .route("/name", http::Method::GET, |_: HttpRequest| EchoServerNameServiceImpl.echo())
+    )
+        .workers(server_setting.workers)
+        .backlog(server_setting.backlog)
+        .keep_alive(server::KeepAlive::Timeout(server_setting.timeout))
+        .bind("0.0.0.0:8080")
+        .unwrap();
 
-        )
-        .workers(100)
-        .backlog(100)
-        .keep_alive(server::KeepAlive::Timeout(75))
-        .bind("0.0.0.0:8080").unwrap()
-        .run();
+    info!("start server application...");
 
+    app_server.run();
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,8 +1,47 @@
 use config::{ConfigError, Config, File};
+use serde;
+use serde::de::Deserializer;
+use serde::Deserialize;
+
+
+pub trait DeserializeWith: Sized {
+    fn deserialize_with<'de, D>(de: D) -> Result<Self, D::Error>
+        where D: Deserializer<'de>;
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub enum ServerMode {
+    Dev,
+    Production
+}
+
+impl DeserializeWith for ServerMode {
+    fn deserialize_with<'de, D>(de: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        let s = String::deserialize(de)?;
+
+        match s.as_ref() {
+            "dev" => Ok(ServerMode::Dev),
+            "production" => Ok(ServerMode::Production),
+            _ => Err(serde::de::Error::custom("not available pattern"))
+        }
+    }
+}
+
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Settings {
-    pub server_name: String
+    pub server_name: String,
+    #[serde(deserialize_with="ServerMode::deserialize_with")]
+    pub mode: ServerMode,
+    pub server: ServerSettings
+
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct ServerSettings {
+    pub timeout: usize,
+    pub workers: usize,
+    pub backlog: i32
 }
 
 impl Settings {
